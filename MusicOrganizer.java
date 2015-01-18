313ad0e64c9d9d81e269d764bf717f0752cbdbb0
Montse <mtschamorro@gmail.com>
2015-01-18 13:54:59 +0100

    modificado constructor para que permita indicar por parametro el nombre de la carpeta donde estan los archivos mp3

--- MusicOrganizer.java
+++ MusicOrganizer.java
@@ -21,13 +21,13 @@ public class MusicOrganizer
     /**
      * Create a MusicOrganizer
      */
-    public MusicOrganizer()
+    public MusicOrganizer(String carpeta)
     {
         tracks = new ArrayList<Track>();
         player = new MusicPlayer();
         reader = new TrackReader();
         playing = false;
-        readLibrary("audio");
+        readLibrary(carpeta);
         System.out.println("Music library loaded. " + getNumberOfTracks() + " tracks.");
         System.out.println();
     }
